3d42c799f5d3619151156156ec8ed48e41f1e361
Brady Dawley <dawleybradym@johndeere.com>
2020-09-11 22:21:01 -0500

    changeImage func added, imgae object fit set to contain

--- script.js
+++ script.js
@@ -2,14 +2,7 @@ const boardSize = [4,5];
 
 const gameBoard = document.querySelector('#gameBoard');
 
-let numRows = "";
 
-for (i = 0; i < boardSize[0];i++) {
-    numRows += '1fr ';  
-}
-numRows = numRows.slice(0,-1);
-gameBoard.style.gridTemplateRows = numRows + ';';
-console.log(gameBoard);
 
 for (i = 0; i < (boardSize[0] * boardSize[1]); i++) {
     const slot = document.createElement('img');
@@ -19,7 +12,20 @@ for (i = 0; i < (boardSize[0] * boardSize[1]); i++) {
     gameBoard.appendChild(slot);
     currentRow = Math.ceil((i+1)/boardSize[1]);
     currentCol = i + 1 - (Math.floor(i/boardSize[1]))*boardSize[1];
-    console.log(currentRow,currentCol);
     slot.style.gridRow = `${currentRow}`;
     slot.style.gridColumn = `${currentCol}`;
 }
+const fakeEvent = document.querySelectorAll('#gameBoard img');
+function changeImage (event) {
+    event[5].setAttribute('src','RedPiece.png')
+}
+changeImage(fakeEvent)
+
+let numRows = "";
+
+for (i = 0; i < boardSize[0];i++) {
+    numRows += '1fr ';  
+}
+numRows = numRows.slice(0,-1);
+//gameBoard.style.gridTemplateRows = numRows + ';';
+console.log(gameBoard);
