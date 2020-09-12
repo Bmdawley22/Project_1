feda711765cfda85b777788387fa8010d7b55336
Brady Dawley <dawleybradym@johndeere.com>
2020-09-11 23:13:12 -0500

    changeUser Function added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const boardSize = [4,5];
+const currentPlayer = 1;
 
 const gameBoard = document.querySelector('#gameBoard');
-
-
+const userTurn = document.querySelectorAll('#userTurn p');
 
 for (i = 0; i < (boardSize[0] * boardSize[1]); i++) {
     const slot = document.createElement('img');
@@ -15,11 +15,36 @@ for (i = 0; i < (boardSize[0] * boardSize[1]); i++) {
     slot.style.gridRow = `${currentRow}`;
     slot.style.gridColumn = `${currentCol}`;
 }
+
 const fakeEvent = document.querySelectorAll('#gameBoard img');
 function changeImage (event) {
-    event[5].setAttribute('src','RedPiece.png')
+    event[5].setAttribute('src','BlackPiece.png')
 }
 changeImage(fakeEvent)
+
+function changeTurn () {
+    if (currentPlayer === 1) {
+        console.log(userTurn[0]);
+        userTurn[0].style.color = '#12130f';
+        userTurn[0].style.backgroundColor = '#12130f';
+        userTurn[0].style.border = '#12130f';
+
+        userTurn[1].style.color = 'white';
+        userTurn[1].style.border = 'grey solid 5px';
+        userTurn[1].style.borderRadius = '10px';
+    }
+    else {
+        userTurn[1].style.color = '#12130f';
+        userTurn[1].style.border = '#12130f';
+
+        userTurn[0].style.color = 'white';
+        userTurn[0].style.backgroundColor = 'rgb(169, 7, 7)';
+        userTurn[0].style.border = 'grey solid 5px';
+        userTurn[0].style.borderRadius = '10px';
+    }
+}
+
+changeTurn();
 
 let numRows = "";
 
