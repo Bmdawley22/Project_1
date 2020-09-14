e3360d0bbc3b980a0c2cd2aa11fb3a664df91b78
Brady Dawley <dawleybradym@johndeere.com>
2020-09-14 11:53:33 -0500

    drop function working

--- script.js
+++ script.js
@@ -39,39 +39,41 @@ function changeArrowImage (event, gamePiece, holdTime) {
         event.style.filter = 'brightness(20%)';
     },holdTime)   
 }
-//Changes images as gamepiece is dropped
-function change2GamePiece(index, gamePiece, holdTime, dropTime) {
-    let lagTime = 20;
-    console.log(holdTime + dropTime + lagTime,index);
-    setTimeout(() => {
-        allSlots[index].setAttribute('src', gamePiece);
-    }, holdTime + dropTime + lagTime);
-    setTimeout(() => {
-        allSlots[index].setAttribute('src', 'BlankPiece.png');
-    }, holdTime + (2 * dropTime) - lagTime); 
-}
 
 function changeTurn () {
-        if (currentPlayer === 1) {
-            userTurn[0].style.color = '#12130f';
-            userTurn[0].style.backgroundColor = '#12130f';
-            userTurn[0].style.border = '#12130f';
+    if (currentPlayer === 1) {
+        userTurn[0].style.color = '#12130f';
+        userTurn[0].style.backgroundColor = '#12130f';
+        userTurn[0].style.border = '#12130f';
 
-            userTurn[1].style.color = 'white';
-            userTurn[1].style.border = 'grey solid 5px';
-            userTurn[1].style.borderRadius = '10px';
-            currentPlayer = 2;
-        }
-        else if (currentPlayer === 2){
-            userTurn[1].style.color = '#12130f';
-            userTurn[1].style.border = '#12130f';
+        userTurn[1].style.color = 'white';
+        userTurn[1].style.border = 'grey solid 5px';
+        userTurn[1].style.borderRadius = '10px';
+        currentPlayer = 2;
+        gamePiece = 'BlackPiece.png';
+    }
+    else if (currentPlayer === 2){
+        userTurn[1].style.color = '#12130f';
+        userTurn[1].style.border = '#12130f';
 
-            userTurn[0].style.color = 'white';
-            userTurn[0].style.backgroundColor = 'rgb(169, 7, 7)';
-            userTurn[0].style.border = 'grey solid 5px';
-            userTurn[0].style.borderRadius = '10px';
-            currentPlayer = 1;
-        }
+        userTurn[0].style.color = 'white';
+        userTurn[0].style.backgroundColor = 'rgb(169, 7, 7)';
+        userTurn[0].style.border = 'grey solid 5px';
+        userTurn[0].style.borderRadius = '10px';
+        currentPlayer = 1;
+        gamePiece = 'RedPiece.png';
+    }
+}
+//Changes images as gamepiece is dropped
+function change2GamePiece(index, i, gamePiece, holdTime, dropTime, rate) {
+    let lagTime = 20;
+    console.log(holdTime + dropTime, holdTime + i * rate + rate);
+    setTimeout(() => {
+        allSlots[index].setAttribute('src', gamePiece);
+    }, holdTime + dropTime);
+    setTimeout(() => {
+        allSlots[index].setAttribute('src', 'BlankPiece.png');
+    }, holdTime + i * rate + rate); 
 }
 
 //Starts game with player 1's turn
@@ -86,18 +88,18 @@ function dropPiece () {
     }
     let indexArr = [];
     indexArr.push(index);
-    for (i = 1; i < boardSize[1]; i++){
-        console.log(index);    
+    for (i = 1; i < boardSize[1]; i++){  
         indexArr.push(indexArr[i-1] + boardSize[1]); 
     }
     let holdTime = 1000;
     changeArrowImage(event.target,gamePiece, holdTime);
     let rate = 100;
-    let dropTime = rate;
+    let dropTime = 0;
     for (i = 0; i < (indexArr.length - 1); i++){
-        change2GamePiece(indexArr[i], gamePiece, holdTime, dropTime);
+        change2GamePiece(indexArr[i], i, gamePiece, holdTime, dropTime ,rate);
         dropTime += rate;
     }
+    console.log(currentPlayer);
     changeTurn();   
 }
 
