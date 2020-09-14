ad6b7e9b3bef4ff5fe76d0ff8682640fcd0806bb
Brady Dawley <dawleybradym@johndeere.com>
2020-09-14 10:53:44 -0500

    Dropping functionality implemented, not finished

--- script.js
+++ script.js
@@ -29,78 +29,90 @@ for (i = 0; i < (boardSize[0] * boardSize[1]); i++) {
 }
 const allSlots = document.querySelectorAll('.slot');
 
-function changeArrowImage (event) {
-    if (currentPlayer === 1) {
-        event.setAttribute('src','RedPiece.png');
-        event.style.filter = 'brightness(100%)';
-        setTimeout(()=>{
-            event.setAttribute('src','Arrow.gif');
-            event.style.filter = 'brightness(20%)';
-        },1000)
+function changeArrowImage (event, gamePiece, holdTime) {
+    event.setAttribute('src','RedPiece.png');
+    event.style.filter = 'brightness(100%)';
+    setTimeout(()=>{
+        event.setAttribute('src','Arrow.gif');
+        event.style.filter = 'brightness(20%)';
+    },holdTime)   
+}
+
+function change2BlankPiece (index) {
+    allSlots[index].setAttribute('src', 'BlankPiece.png');
+}
+function change2GamePiece(index, gamePiece, holdTime, dropTime) {
+
+    if (index < (gameBoard[1] - 1)) {
+        setTimeout(() => {
+            allSlots[index].setAttribute('src', gamePiece);
+        }, holdTime);
+        setTimeout(() => {
+            allSlots[index].setAttribute('src', 'BlankPiece.png');
+        }, holdTime + dropTime);
     } else {
-        event.setAttribute('src','BlackPiece.png');
-        event.style.filter = 'brightness(100%)';
-        setTimeout(()=>{
-            event.setAttribute('src','Arrow.gif');
-            event.style.filter = 'brightness(20%)';
-        },1000)
+        setTimeout(() => {
+            allSlots[index].setAttribute('src', gamePiece);
+        }, holdTime + dropTime);
+        setTimeout(() => {
+            allSlots[index].setAttribute('src', 'BlankPiece.png');
+        }, holdTime + 2*dropTime);
     }
     
 }
-function moveImage (index) {
+
+function changeTurn (totalTime) {
+    setTimeout(() => {
+        if (currentPlayer === 1) {
+            userTurn[0].style.color = '#12130f';
+            userTurn[0].style.backgroundColor = '#12130f';
+            userTurn[0].style.border = '#12130f';
+
+            userTurn[1].style.color = 'white';
+            userTurn[1].style.border = 'grey solid 5px';
+            userTurn[1].style.borderRadius = '10px';
+            currentPlayer = 2;
+        }
+        else if (currentPlayer === 2){
+            userTurn[1].style.color = '#12130f';
+            userTurn[1].style.border = '#12130f';
+
+            userTurn[0].style.color = 'white';
+            userTurn[0].style.backgroundColor = 'rgb(169, 7, 7)';
+            userTurn[0].style.border = 'grey solid 5px';
+            userTurn[0].style.borderRadius = '10px';
+            currentPlayer = 1;
+        }
+        }, totalTime);
+}
+
+//Starts game with player 1's turn
+changeTurn();
+
+function dropPiece () {
+    const index = Array.from(event.target.parentElement.children).indexOf(event.target);
     if (currentPlayer === 1) {
         gamePiece = 'RedPiece.png';
     } else {
         gamePiece = 'BlackPiece.png';
     }
-    //for (i = 0; i < boardSize[1]; i++){
-        console.log(index);
-        change2BlankPiece(index)
-        index += boardSize[1];
-    
-        
-            
-}
-async function change2BlankPiece (index) {
-    const wait = await setTimeout(change2GamePiece(index,gamePiece),2000)
-    allSlots[index].setAttribute('src', 'BlankPiece.png');
-}
-function change2GamePiece(index,gamePiece) {
-    console.log(allSlots[index])
-    allSlots[index].setAttribute('src', gamePiece);
-}
-
-function changeTurn () {
-    if (currentPlayer === 1) {
-        userTurn[0].style.color = '#12130f';
-        userTurn[0].style.backgroundColor = '#12130f';
-        userTurn[0].style.border = '#12130f';
-
-        userTurn[1].style.color = 'white';
-        userTurn[1].style.border = 'grey solid 5px';
-        userTurn[1].style.borderRadius = '10px';
-        currentPlayer = 2;
+    let indexArr = [];
+    indexArr.push(index);
+    for (i = 1; i < boardSize[1]; i++){
+        console.log(index);    
+        indexArr.push(indexArr[i-1] + boardSize[1]); 
     }
-    else if (currentPlayer === 2){
-        userTurn[1].style.color = '#12130f';
-        userTurn[1].style.border = '#12130f';
-
-        userTurn[0].style.color = 'white';
-        userTurn[0].style.backgroundColor = 'rgb(169, 7, 7)';
-        userTurn[0].style.border = 'grey solid 5px';
-        userTurn[0].style.borderRadius = '10px';
-        currentPlayer = 1;
+    let holdTime = 1000;
+    let totalTime = holdTime;
+    changeArrowImage(event.target,gamePiece, holdTime);
+    let dropTime = 0;
+    let rate = 100
+    for (i = 0; i < indexArr.length; i++){
+        change2GamePiece(indexArr[i], gamePiece, holdTime, dropTime);
+        dropTime += rate;
     }
-}
-
-changeTurn();
-
-function dropPiece () {
-    const index = Array.from(event.target.parentElement.children).indexOf(event.target);
-    changeArrowImage(event.target);
-    moveImage(index);
-    setTimeout(changeTurn,2000);
-
+    totalTime += dropTime;
+    changeTurn(totalTime);   
 }
 
 
@@ -115,3 +127,4 @@ numRows = numRows.slice(0,-1);
 numRows = numRows + ';';
 console.log(numRows);
 */
+
